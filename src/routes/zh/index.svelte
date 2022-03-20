766b05509c11278499ec7c495de1267ea28e60c9
joeleung32 <joeleung32@yahoo.com.hk>
2022-03-20 20:32:40 +0800

    Sampling for marked

--- src/routes/zh/index.svelte
+++ src/routes/zh/index.svelte
@@ -4,10 +4,12 @@
 
     // Libraries
     import {locale, t} from '$lib/translations';
+    import {marked} from 'marked';
 </script>
 <div class="container">
     <Navbar/>
     <div class="my-5">
         <h1>{$t('home.welcome')}</h1>
+        <p>{@html marked(`樣式內容，使用了 **marked** 庫.`)}</p>
     </div>
 </div>
